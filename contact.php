f11d90190f6c9ef0badd38724b4882959d8d067c
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2021-02-03 13:59:36 +0100

    add param smtp

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
 /*
 THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
+AND ALSO SMTP TO SEND THE EMAILS
 */
 
 require 'PHPMailer-master/PHPMailerAutoload.php';
@@ -37,10 +38,10 @@ $errorMessage = 'There was an error while submitting the form. Please try again 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
-try
-{
-    
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
+try {
+    if (count($_POST) == 0) {
+        throw new \Exception('Form is empty');
+    }
     
     $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
     $emailTextHtml .= "<table>";
@@ -63,31 +64,57 @@ try
     $mail->isHTML(true);
     
     $mail->Subject = $subject;
+    $mail->Body    = $emailTextHtml;
     $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
     
     
-    if(!$mail->send()) {
+    $mail->isSMTP();
+    
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 0;
+    $mail->Debugoutput = 'html';
+    
+    //Set the hostname of the mail server
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
+    $mail->Host = gethostbyname($smtpHost);
+    
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = $smtpHost;
+    
+    //Password to use for SMTP authentication
+    $mail->Password = $smtpPassword;
+    
+    if (!$mail->send()) {
         throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
     }
     
     $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
+} catch (\Exception $e) {
     // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
     $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
 }
 
 
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-    
-    header('Content-Type: application/json');
-    
-    echo $encoded;
+if ($responseArray['type'] == 'success') {
+    // success redirect
+
+    header('Location: http://www.example.com/success.html');
 }
-// else just display the message
 else {
-    echo $responseArray['message'];
+    //error redirect
+    header('Location: http://www.example.com/error.html');
 }
