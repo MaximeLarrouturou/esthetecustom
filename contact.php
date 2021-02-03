548b57370aeef906200ffba0dc244d487735c0c0
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2021-02-03 14:09:14 +0100

    reload old version

--- contact.php
+++ contact.php
@@ -115,12 +115,15 @@ try {
 }
 
 
-if ($responseArray['type'] == 'success') {
-    // success redirect
-
-    header('Location: http://www.example.com/success.html');
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
 }
+// else just display the message
 else {
-    //error redirect
-    header('Location: http://www.example.com/error.html');
+    echo $responseArray['message'];
 }
